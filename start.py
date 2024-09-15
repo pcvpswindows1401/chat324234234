c38f5ab45da99c8a21cf3f92977d16499366c06b
pcvpswindows1401 <135259492+pcvpswindows1401@users.noreply.github.com>
2024-09-15 16:53:43 +0330

    Create start.py

---
+++ start.py
@@ -0,0 +1,20 @@
+import threading , time , os
+
+open("check.txt","w").write("False")
+
+def st():
+  while True:
+    time.sleep(2)
+    print("Starting...")
+    os.system("python bot.py")
+
+t = threading.Thread(target=st)
+
+
+t.start()
+while True:
+  time.sleep(3)
+  f = open("check.txt","r").read()
+  if f == "True":break
+    
+print("End")
