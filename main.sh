730b478878413c4fe7e0becde2df78ba2b622cea
pcvpswindows1401 <135259492+pcvpswindows1401@users.noreply.github.com>
2024-09-15 17:01:47 +0330

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Infinite loop to run the Python script
+while true
+do
+    # Run the Python script
+    python start.py
+    # Sleep for a certain amount of time (optional)
+    sleep 1
+done
